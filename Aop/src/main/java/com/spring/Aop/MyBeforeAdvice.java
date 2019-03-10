a7909b08cdde8095a6e31fd9f8292bb87bd7cd33
youisdx <1601764856qq.com>
2019-03-10 13:59:44 +0800

    commit  daily update

--- Aop/src/main/java/com/spring/Aop/MyBeforeAdvice.java
+++ Aop/src/main/java/com/spring/Aop/MyBeforeAdvice.java
@@ -1,7 +1,18 @@
 package com.spring.Aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+/**
+ * 用户自定义的前置增强对象
+ */
 public class MyBeforeAdvice {
+    private static final Logger logger=  LoggerFactory.getLogger(MyBeforeAdvice.class);
     public void beforeMethod(){
-        System.out.println("This is a before method");
+
+        logger.debug("this is a before pfz");
+//        System.out.println("This is a before method");
     }
 }
